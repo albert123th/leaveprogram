17c90e81c5c6920f2c2a8e81e6d3ef59904296a4
Athv <71397557+albert123th@users.noreply.github.com>
2024-03-20 22:26:57 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function doPost(e) {
-    var ss = SpreadsheetApp.openById('YOUR_SPREADSHEET_ID');
+    var ss = SpreadsheetApp.openById('AKfycby_hlV1Aj4j93tCOGJiQqyet1pO6aFOFm7CQ0z8Q_uygfBZY7DB2-rMS6CdeEhRdbo');
     var sheet = ss.getSheetByName('Sheet1'); // Change 'Sheet1' to the name of your sheet
     var data = JSON.parse(e.postData.contents);
 
